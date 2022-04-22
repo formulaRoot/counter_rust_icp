037f2045157d12fbc8ab686c98e1717c1043e6d1
formula{Root} <root.formula1@gmail.com>
2022-04-22 08:19:29 -0400

    first commit

--- src/hack/src/lib.rs
+++ src/hack/src/lib.rs
@@ -1,4 +1,27 @@
-#[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk_macros::*;
+
+
+
+#[query]
+fn get_count() -> u32 {
+    let state: &State = ic_cdk::storage::get();
+    state.count
+}
+
+#[update]
+fn count() -> u32 {
+    let state: &mut State = ic_cdk::storage::get_mut();
+    state.count += 1;
+    state.count
+}
+
+#[update]
+fn reset() {
+    let state: &mut State = ic_cdk::storage::get_mut();
+    std::mem::take(state);
+}
+
+#[derive(Default)]
+struct State {
+    count: u32,
 }
